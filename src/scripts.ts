703579bd05c65171af15f82052a311c9b4a04d63
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-01-31 10:32:21 +0200

    added jQery and comments about taks

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,15 @@
 console.log('Ready for coding');
+
+//To do
+//  1)Create form using jQuery
+//      a)username
+//      b)email  
+//      b)password  
+//  2)validate  
+//      a)Name: The name must have at least 2 characters and can only contain letters. Max 50 characters.
+//      b)Email: The email must be in a valid format (e.g. example@example.com).
+//      c)Password: The password must be at least 8 characters long and must contain at least 1 number and 1 special character (!, @, #, $, %, ^, &, *).
+//      d)When the user submits the form, the validation rules should be checked. If all fields are valid, show a success message. If any field is invalid, show an error message for that field.
+//      e)When the user submits the form, the validation rules should be checked. If all fields are valid, show a success message. If any field is invalid, show an error message for that field.
+//  3)Uzrakstīt test case'us priekš validācijas funkcijām ar Jest
+
